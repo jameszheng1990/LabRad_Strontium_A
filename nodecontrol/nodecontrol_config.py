539398f22fd03b553c1b6a2270c07554df04b048
jameszheng1990 <53911713+jameszheng1990@users.noreply.github.com>
2020-03-05 14:17:24 -0600

    Add files via upload

---
+++ nodecontrol/nodecontrol_config.py
@@ -0,0 +1,25 @@
+node_dict = {
+    'node_sra': [
+        'update',
+        
+        'picoscope',
+        'serial',
+        'visa',
+        'vxi11',
+        'socket',
+
+#        'andor2'        
+        'afg',
+        'camera',
+        'rf',
+        'pd',
+        'pmt',
+        'tcam',
+        
+        'msquared',
+        'ni',
+        'sequencer',
+        'conductor',
+        ],
+    }
+
---
+++ nodecontrol/nodecontrol_start.py
@@ -0,0 +1,26 @@
+import labrad 
+import numpy as np
+import time
+from nodecontrol_config import node_dict
+import os
+
+cxn = labrad.connect(host=os.getenv('LABRADHOST'), password='')
+for node in node_dict.keys():
+    if node in cxn.servers: 
+        print('{}:'.format(node))
+        cxn.servers[node].refresh_servers()
+        running_servers = np.array(cxn.servers[node].running_servers())
+        for server in node_dict[node]:
+            if server in running_servers: 
+                print('{} is running'.format(server))
+            else:
+                print('starting ' + server)
+                try:
+                    cxn.servers[node].start(server)
+                except Exception as e:
+                    print('error with ' + server)
+                    print(e)
+    else:
+        print('{} is not running'.format(node))
+
+print('done!')
