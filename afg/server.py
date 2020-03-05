d8ece9baff23bf95d2aed2ce16c53e1479d9312f
jameszheng1990 <53911713+jameszheng1990@users.noreply.github.com>
2020-03-05 10:49:55 -0600

    Add files via upload

--- afg/server.py
+++ afg/server.py
@@ -41,22 +41,22 @@ class AFGServer(DeviceServer):
             active_devices = self._get_active_devices()
             request = {device_name: None for device_name in active_devices}
         response = {}
-        for device_name, waveform in request.items():
+        for device_name, req in request.items():
             device_response = None
             try:
-                device_response = self._waveform(device_name, waveform)
+                device_response = self._waveform(device_name, req['source'], req['wfm'])
             except:
                 self._reload_device(device_name, {})
-                device_response = self._waveform(device_name, waveform)
+                device_response = self._waveform(device_name, req['source'], req['wfm'])
             response.update({device_name: device_response})
-        self._send_update({'waveforms': response})
+        self._send_update({'waveforms': response, 'source': req['source']})
         return response
 
-    def _waveform(self, name, waveform):
+    def _waveform(self, name, source, waveform):
         device = self._get_device(name)
         if waveform:
-            device.set_waveform(waveform)
-        response = device.get_waveform()
+            device.set_waveform(source, waveform)
+        response = device.get_waveform(source)
         return response
     
     @setting(11)
@@ -68,10 +68,39 @@ class AFGServer(DeviceServer):
     def _run(self, request):
         for device_name, request in request.items():
             device = self._get_device(device_name)
-            run_state = device.get_runstate()
-            if run_state:
-                device.stop()
+            device.stop()
             device.run()
+            
+    @setting(12)
+    def scales(self, c, request_json='{}'):
+        """ set scales """
+        request = json.loads(request_json)
+        response = self._scales(request)
+        response_json = json.dumps(response)
+        return response_json
+
+    def _scales(self, request):
+        if request == {}:
+            active_devices = self._get_active_devices()
+            request = {device_name: None for device_name in active_devices}
+        response = {}
+        for device_name, req in request.items():
+            device_response = None
+            try:
+                device_response = self._scale(device_name, req['source'], req['scale'])
+            except:
+                self._reload_device(device_name, {})
+                device_response = self._scale(device_name, req['source'], req['scale'])
+            response.update({device_name: device_response})
+        self._send_update({'scales': response, 'source': req['source']})
+        return response
+
+    def _scale(self, name, source, scale):
+        device = self._get_device(name)
+        if scale:
+            device.set_scale(source, scale)
+        response = device.get_scale(source)
+        return response
 
 Server = AFGServer
 
