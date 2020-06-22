f00e9575034f4ac081c2a4dd379e0fd9eb4616ec
jameszheng1990 <53911713+jameszheng1990@users.noreply.github.com>
2020-06-22 09:10:33 -0500

    Add files via upload

---
+++ conductor/parameters/sequencer/AO_parameters/Lattice 813A AM.py
@@ -0,0 +1,33 @@
+import json
+import numpy as np
+import os
+import time
+import traceback
+
+from twisted.internet.defer import inlineCallbacks
+
+from conductor.parameter import ConductorParameter
+
+class Lattice_813A_AM(ConductorParameter):
+    autostart = False
+    priority = 1
+    call_in_thread = True
+    value = None
+    value_type = 'single'
+
+    def initialize(self, config):
+        super(Lattice_813A_AM, self).initialize(config)
+
+    def update(self):
+        experiment = self.server.experiment
+        is_end = self.server.is_end 
+        try:    
+            parameter_values = self.server.experiment['parameter_values']
+            if (experiment is not None) and  (not is_end) and ('Lattice 813A AM' in parameter_values['sequencer.AO_parameters']) :
+                pass
+            else:
+                self.value = None
+        except:
+            self.value = None
+
+Parameter = Lattice_813A_AM
