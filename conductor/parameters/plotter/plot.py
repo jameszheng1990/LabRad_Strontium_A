f36a0faf8292eaa912d786b4f76ae844dc85055f
jameszheng1990 <53911713+jameszheng1990@users.noreply.github.com>
2020-07-20 12:04:08 -0500

    Add files via upload

--- conductor/parameters/plotter/plot.py
+++ conductor/parameters/plotter/plot.py
@@ -9,7 +9,6 @@ class Plot(ConductorParameter):
     call_in_thread = True
     value_type = 'dict'
     value = None
-    shot = None
     
     plotter_servername = 'plotter'
 
@@ -17,28 +16,25 @@ class Plot(ConductorParameter):
         self.connect_to_labrad()
     
     def update(self):
-        is_first = self.server.is_first
+        """ 
+        This parameter plots after each shot, so we need to know previous experiment.
+        For the first shot, the previous_experiment should be {}.
+        """
         is_end = self.server.is_end 
-        experiment_name = self.server.experiment.get('name')
-        shot_number = self.server.experiment.get('shot_number')
-
-        if self.value and is_first and (experiment_name is not None) and (shot_number is not None):
-            self.shot = shot_number - 1
-            self.value['data_path'] = experiment_name
-            self.value['shot'] = self.shot
-            self.value['to_plot'] = False
         
-        elif self.value and (not is_end):
-            self.shot = shot_number - 1
-            self.value['shot'] = self.shot
+        previous_experiment_name = self.server.previous_experiment.get('name')
+        previous_shot_number = self.server.previous_experiment.get('shot_number')
+        
+        self.value['data_path'] = previous_experiment_name
+        self.value['shot'] = previous_shot_number
+            
+        if self.value and not (is_end) and (previous_experiment_name is not None):
+            self.value['to_plot'] = False
             self.cxn.plotter.plot(json.dumps(self.value))
         
         elif self.value and is_end:
-            self.shot += 1
-            self.value['shot'] = self.shot
             self.value['to_plot'] = True
             self.cxn.plotter.plot(json.dumps(self.value))
             self.value = None
-            self.shot = None
 
 Parameter = Plot
