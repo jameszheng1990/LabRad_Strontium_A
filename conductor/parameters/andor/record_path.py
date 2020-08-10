c108dee21df2aa2e3570c96fbbc539c7478436de
jameszheng1990 <53911713+jameszheng1990@users.noreply.github.com>
2020-08-10 11:21:17 -0500

    Add files via upload

--- conductor/parameters/andor/record_path.py
+++ conductor/parameters/andor/record_path.py
@@ -9,69 +9,87 @@ class RecordPath(ConductorParameter):
     autostart = False
     priority = 2
     record_sequences = {
-        'image': 'g',
-        'image_OSGtransfer': 'g',
-        'image_3P1_excitation': 'g',
-        'image_3P1_excitation-405': 'g',
-        'image_v2': 'g',
-        'image_clock': 'eg',
-        'image_ft': 'eg',
-        'image-fast': 'fast-g',
-        'image-clock-fast': 'fast-eg',
-        'image-clock-fast-side': 'fast-eg',
+        # 'image': 'g',
+        # 'image_OSGtransfer': 'g',
+        # 'image_3P1_excitation': 'g',
+        # 'image_3P1_excitation-405': 'g',
+        # 'image_v2': 'g',
+        # 'image_clock': 'eg',
+        # 'image_ft': 'eg',
+        # 'image-fast': 'fast-g',
+        # 'image-clock-fast': 'fast-eg',
+        # 'image-clock-fast-side': 'fast-eg',
+        
+        'image_test': 'test',
+        
+        'image_absorption_blue' : 'g_abs_img',
+        'image_absorption_red' : 'g_abs_img',
+        'image_absorption_red_fast' : 'g_abs_img',
+        'image_absorption_lattice' : 'g_abs_img',
+        
+        'image_fluorescence_lattice': 'g_fluo_img',
         }
-
-    data_filename = '{}.ikon'
-    nondata_filename = '{}/ikon'
+    
+    data_filename = '{}.ixon'
+    nondata_filename = '{}/ixon'
+    
+    device_name = 'ixon'
 
     record_settings = {}
+    value_type = 'dict'
 
     def initialize(self, config):
         super(RecordPath, self).initialize(config)
         self.connect_to_labrad()
 
-        request = {'microscope': {}}
-        self.cxn.camera.initialize_devices(json.dumps(request))
+        # request = {self.device_name: {}}
+        # self.cxn.camera.initialize_devices(json.dumps(request))
     
     @property
-    def value(self):
+    def path(self):
         experiment_name = self.server.experiment.get('name')
         shot_number = self.server.experiment.get('shot_number')
         sequence = self.server.parameters.get('sequencer.sequence')
-        previous_sequence = self.server.parameters.get('sequencer.previous_sequence')
-
-        value = None
+        # previous_sequence = self.server.parameters.get('sequencer.previous_sequence')
+        
+        path = None
         rel_point_path = None
         self.record_type = None
+        
         if (experiment_name is not None) and (sequence is not None):
             point_filename = self.data_filename.format(shot_number)
             rel_point_path = os.path.join(experiment_name, point_filename)
         elif sequence is not None:
             rel_point_path = self.nondata_filename.format(time.strftime('%Y%m%d'))
         
-        if sequence.loop:
-            if np.intersect1d(previous_sequence.value, self.record_sequences.keys()):
-                intersection = np.intersect1d(previous_sequence.value, self.record_sequences.keys())
-                value = rel_point_path
-                self.record_type = self.record_sequences[intersection[-1]]
-        elif np.intersect1d(sequence.value, self.record_sequences.keys()):
-            intersection = np.intersect1d(sequence.value, self.record_sequences.keys())
-            value = rel_point_path
+        if np.intersect1d(sequence.value, list(self.record_sequences.keys())):
+            intersection = np.intersect1d(sequence.value, list(self.record_sequences.keys()))
+            path = rel_point_path
             self.record_type = self.record_sequences[intersection[-1]]
 
-        return value
+        return path
     
-    @value.setter
-    def value(self, x):
+    @path.setter
+    def path(self, x):
         pass
     
     def update(self):
-        if self.value is not None:
+        is_end = self.server.is_end
+        parameter_values = self.server.experiment.get('parameter_values')
+        
+        if self.value['roi']:
+            self.record_settings['xi'] = self.value.get('roi')[0]
+            self.record_settings['xf'] = self.value.get('roi')[1]
+            self.record_settings['yi'] = self.value.get('roi')[2]
+            self.record_settings['yf'] = self.value.get('roi')[3]
+            
+        if (self.path is not None) and (not is_end) and ('andor' in parameter_values):
+            
             request = {
-                'microscope': {
+                self.device_name: {
                     'record': {
                         'kwargs': {
-                            'record_path': self.value,
+                            'record_path': self.path,
                             'record_type': self.record_type,
                             'record_settings': self.record_settings,
                             },
@@ -79,70 +97,5 @@ class RecordPath(ConductorParameter):
                     },
                 }
             self.cxn.camera.call_in_thread(json.dumps(request))
-
-#class RecordPath(ConductorParameter):
-#    autostart = False
-#    priority = 2
-#    record_sequences = {
-#        'image': 'record_g',
-#        'image_OSGtransfer': 'record_g',
-#        'image_3P1_excitation': 'record_g',
-#        'image_3P1_excitation-405': 'record_g',
-#        'image_v2': 'record_g',
-#        'image_clock': 'record_eg',
-#        'image_ft': 'record_eg',
-#        }
-#
-#    data_filename = '{}.ikon'
-#    nondata_filename = '{}/ikon'
-#
-#    image_settings = {}
-#
-#    def initialize(self, config):
-#        print 'initing andor.record_path'
-#        super(RecordPath, self).initialize(config)
-#        self.connect_to_labrad()
-#        self.cxn.yesr10_andor.select_device('hr_ikon')
-#    
-#    @property
-#    def value(self):
-#        experiment_name = self.server.experiment.get('name')
-#        shot_number = self.server.experiment.get('shot_number')
-#        sequence = self.server.parameters.get('sequencer.sequence')
-#        previous_sequence = self.server.parameters.get('sequencer.previous_sequence')
-#
-#        value = None
-#        rel_point_path = None
-#        self.recorder_name = None
-#        if (experiment_name is not None) and (sequence is not None):
-#            point_filename = self.data_filename.format(shot_number)
-#            rel_point_path = os.path.join(experiment_name, point_filename)
-#        elif sequence is not None:
-#            rel_point_path = self.nondata_filename.format(time.strftime('%Y%m%d'))
-#
-#        
-#        if sequence.loop:
-#            if np.intersect1d(previous_sequence.value, self.record_sequences.keys()):
-#                intersection = np.intersect1d(previous_sequence.value, self.record_sequences.keys())
-#                value = rel_point_path
-#                self.recorder_name = self.record_sequences[intersection[-1]]
-#        elif np.intersect1d(sequence.value, self.record_sequences.keys()):
-#            intersection = np.intersect1d(sequence.value, self.record_sequences.keys())
-#            value = rel_point_path
-#            self.recorder_name = self.record_sequences[intersection[-1]]
-#
-#        return value
-#    
-#    @value.setter
-#    def value(self, x):
-#        pass
-#    
-#    def update(self):
-#        if self.value is not None:
-#            print self.value
-#            image_settings_json = json.dumps(self.image_settings)
-#            self.cxn.yesr10_andor.record(self.value, self.recorder_name, image_settings_json)
-#        else:
-#            print self.nondata_filename
-
+            
 Parameter = RecordPath
