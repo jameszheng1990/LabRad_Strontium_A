08d6123eed3975e01532f35c075ac7d0aa0fb1d2
jameszheng1990 <53911713+jameszheng1990@users.noreply.github.com>
2020-08-31 15:06:01 -0500

    Add files via upload

--- camera/devices/ixon.py
+++ camera/devices/ixon.py
@@ -21,65 +21,59 @@ class ixon(AndorCam):
         
         andor.Initialize()
         andor.SetFanMode(0) # Fan on full
-        andor.SetTemperature(-65)
+        andor.SetTemperature(-70)
         andor.SetCoolerMode(0)
         andor.CoolerON()
-    
-    def record(self, record_path=None, record_type=None, record_settings={}):
+        
+    def config(self, record_path=None, record_type=None, record_settings={}):
         if record_path is None:
             return
         
         andor_server = self.cxn[self.andor_servername]
         andor = AndorProxy(andor_server)
-
-        # # abort if acquiring
-        andor.AbortAcquisition()
         
         try:
             xi = record_settings['roi'][0]
             xf = record_settings['roi'][1]
             yi = record_settings['roi'][2]
             yf = record_settings['roi'][3]
-            em_gain = record_settings['em_gain']
-            em_gain = sorted([self.min_em_gain, em_gain, self.max_em_gain])[1]
+            
         except:
             xi = 1
             xf = 1024
             yi = 1
             yf = 1024
-            em_gain = self.min_em_gain
             
+        try:
+            if record_settings['em_gain'] < 2:
+                em_mode = 0
+                em_gain = 0
+            else:
+                em_mode = 3
+                em_gain = record_settings['em_gain']
+                em_gain = sorted([self.min_em_gain, int(em_gain), self.max_em_gain])[1]
+            
+        except:
+            em_mode = 0
+            em_gain = 0
+        
         if record_type == 'g_abs_img':
-            number_of_img = 2
             andor.SetAcquisitionMode(3) # Kinetics mode
             andor.SetReadMode(4) # Image mode
             andor.SetNumberAccumulations(1)
-            andor.SetNumberKinetics(number_of_img) # 2 images each run
+            andor.SetNumberKinetics(3) # 3 images each run
             andor.SetAccumulationCycleTime(0)
             andor.SetKineticCycleTime(0)
             andor.SetPreAmpGain(0)
-            andor.SetEMGainMode(4)  # Real value mode
+            andor.SetEMGainMode(em_mode)  # 3 for Real value mode
             andor.SetEMCCDGain(em_gain)
             andor.SetHSSpeed(0, 0)
             andor.SetVSSpeed(1)
             andor.SetShutter(1, 1, 0, 0)  # permanently open
             andor.SetTriggerMode(1)
             andor.SetExposureTime(500e-6)
-            # andor.SetImage(1, 1, 1, 1024, 1, 1024)
             andor.SetImage(1, 1, xi, xf, yi, yf)
-            
-            for i in range(number_of_img):
-                andor.StartAcquisition()
-                andor.WaitForAcquisition()
-            
-            # data = andor.GetAcquiredData(3 * 1024 * 1024).reshape(3, 1024, 1024)
-            data = andor.GetAcquiredData(number_of_img * (xf-xi+1) * (yf-yi+1)).reshape(number_of_img,  (yf-yi+1), (xf-xi+1))
-            images = {key: data[i] 
-                      for i, key in enumerate(["image", "bright"])}
-            
-            self._save(images, record_path)
-            self._send_update(record_path, record_type)
-        
+    
         if record_type == 'eg_abs_img':
             andor.SetAcquisitionMode(4)
             andor.SetReadMode(4)
@@ -89,7 +83,115 @@ class ixon(AndorCam):
             andor.SetShutter(1, 1, 0, 0)
             andor.SetTriggerMode(1)
             andor.SetFastKineticsEx(341, 3, 500e-6, 4, 1, 1, 682)
+            
+        if record_type == 'fast-g_abs_img':
+            andor.SetAcquisitionMode(4)
+            andor.SetReadMode(4)
+            andor.SetPreAmpGain(0)
+            andor.SetHSSpeed(0, 0)
+            andor.SetVSSpeed(1)
+            andor.SetShutter(1, 1, 0, 0)
+            andor.SetTriggerMode(1)
+            andor.SetFastKineticsEx(341, 3, 500e-6, 4, 1, 1, 683)
 
+        if record_type == 'fast-eg_abs_img':
+            andor.SetAcquisitionMode(4)
+            andor.SetReadMode(4)
+            andor.SetPreAmpGain(0)
+            andor.SetHSSpeed(0, 0)
+            andor.SetVSSpeed(1)
+            andor.SetShutter(1, 1, 0, 0)
+            andor.SetTriggerMode(1)
+            andor.SetFastKineticsEx(256, 4, 500e-6, 4, 1, 1, 1024 - 256)
+#            andor.SetFastKinetics(256, 4, 500e-6, 4, 1, 1)
+
+        if record_type == 'g_fluo_img':
+            andor.SetAcquisitionMode(3) # Kinetics mode
+            andor.SetReadMode(4) # Image mode
+            andor.SetNumberAccumulations(1)
+            andor.SetNumberKinetics(2) # 2 images each run
+            andor.SetAccumulationCycleTime(0)
+            andor.SetKineticCycleTime(0)
+            andor.SetPreAmpGain(0)
+            andor.SetEMGainMode(em_mode)  # 3 for Real value mode
+            andor.SetEMCCDGain(em_gain)
+            andor.SetHSSpeed(0, 0)
+            andor.SetVSSpeed(1)
+            andor.SetShutter(1, 1, 0, 0)  # permanently open
+            andor.SetTriggerMode(1)
+            andor.SetExposureTime(1e-3)
+            andor.SetImage(1, 1, xi, xf, yi, yf)
+            
+        if record_type == 'raw':
+            andor.SetAcquisitionMode(3) # Kinetics mode
+            andor.SetReadMode(4) # Image mode
+            andor.SetNumberAccumulations(1)
+            andor.SetNumberKinetics(2) # 2 images each run
+            andor.SetAccumulationCycleTime(0)
+            andor.SetKineticCycleTime(0)
+            andor.SetPreAmpGain(0)
+            andor.SetEMGainMode(em_mode)  # 3 for Real value mode
+            andor.SetEMCCDGain(em_gain)
+            andor.SetHSSpeed(0, 0)
+            andor.SetVSSpeed(1)
+            andor.SetShutter(1, 1, 0, 0)  # permanently open
+            andor.SetTriggerMode(1)
+            andor.SetExposureTime(1e-3)
+            andor.SetImage(1, 1, xi, xf, yi, yf)
+            
+        if record_type == 'gain_calibration':
+            andor.SetAcquisitionMode(3) # Kinetics mode
+            andor.SetReadMode(4) # Image mode
+            andor.SetNumberAccumulations(1)
+            andor.SetNumberKinetics(3) # number of images each run
+            andor.SetAccumulationCycleTime(0)
+            andor.SetKineticCycleTime(0)
+            andor.SetEMGainMode(em_mode)  # 3 for Real value mode
+            andor.SetEMCCDGain(em_gain)
+            andor.SetPreAmpGain(0)
+            andor.SetHSSpeed(0, 0)
+            andor.SetVSSpeed(1)
+            andor.SetShutter(1, 1, 0, 0)  # permanently open
+            andor.SetTriggerMode(1)
+            andor.SetExposureTime(1e-3)
+            andor.SetImage(1, 1, xi, xf, yi, yf)
+            
+    def record(self, record_path=None, record_type=None, record_settings={}):
+        if record_path is None:
+            return
+        
+        andor_server = self.cxn[self.andor_servername]
+        andor = AndorProxy(andor_server)
+
+        # # abort if waiting
+        andor.CancelWait()
+        # andor.AbortAcquisition()
+        
+        try:
+            xi = record_settings['roi'][0]
+            xf = record_settings['roi'][1]
+            yi = record_settings['roi'][2]
+            yf = record_settings['roi'][3]
+            
+        except:
+            xi = 1
+            xf = 1024
+            yi = 1
+            yf = 1024
+            
+        if record_type == 'g_abs_img':
+            for i in range(3):
+                andor.StartAcquisition()
+                andor.WaitForAcquisition()
+            
+            data = andor.GetAcquiredData(3 * (xf-xi+1) * (yf-yi+1)).reshape(3,  (yf-yi+1), (xf-xi+1))
+            images = {key: data[i] 
+                      for i, key in enumerate(["image", "bright", "dark"])}
+            
+            self._save(images, record_path)
+            self._send_update(record_path, record_type, record_settings)
+        
+        if record_type == 'eg_abs_img':
             andor.StartAcquisition()
             andor.WaitForAcquisition()
             data = andor.GetAcquiredData(3 * 341 * 1024).reshape(3, 341, 1024)
@@ -106,18 +208,9 @@ class ixon(AndorCam):
             images.update(background_images)
             
             self._save(images, record_path)
-            self._send_update(record_path, record_type)
+            self._send_update(record_path, record_type, record_settings)
         
-        if record_type == 'fast-g_abs':
-            andor.SetAcquisitionMode(4)
-            andor.SetReadMode(4)
-            andor.SetPreAmpGain(0)
-            andor.SetHSSpeed(0, 0)
-            andor.SetVSSpeed(1)
-            andor.SetShutter(1, 1, 0, 0)
-            andor.SetTriggerMode(1)
-            andor.SetFastKineticsEx(341, 3, 500e-6, 4, 1, 1, 683)
-
+        if record_type == 'fast-g_abs_img':
             andor.StartAcquisition()
             andor.WaitForAcquisition()
             data = andor.GetAcquiredData(3 * 341 * 1024).reshape(3, 341, 1024)
@@ -125,19 +218,9 @@ class ixon(AndorCam):
                     for i, key in enumerate(["image", "bright", "dark"])}
             
             self._save(images, record_path)
-            self._send_update(record_path, record_type)
+            self._send_update(record_path, record_type, record_settings)
         
-        if record_type == 'fast-eg_abs':
-            andor.SetAcquisitionMode(4)
-            andor.SetReadMode(4)
-            andor.SetPreAmpGain(0)
-            andor.SetHSSpeed(0, 0)
-            andor.SetVSSpeed(1)
-            andor.SetShutter(1, 1, 0, 0)
-            andor.SetTriggerMode(1)
-            andor.SetFastKineticsEx(256, 4, 500e-6, 4, 1, 1, 1024 - 256)
-#            andor.SetFastKinetics(256, 4, 500e-6, 4, 1, 1)
-
+        if record_type == 'fast-eg_abs_img':
             andor.StartAcquisition()
             andor.WaitForAcquisition()
             data = andor.GetAcquiredData(4 * 256 * 1024).reshape(4, 256, 1024)
@@ -146,8 +229,44 @@ class ixon(AndorCam):
                     for i, key in enumerate(["image-g", "image-e", "bright", "empty"])}
             
             self._save(images, record_path)
-            self._send_update(record_path, record_type)
+            self._send_update(record_path, record_type, record_settings)
         
-        print('Andor IXon-888 recorded.')
+        if record_type == 'g_fluo_img':
+            for i in range(2):
+                andor.StartAcquisition()
+                andor.WaitForAcquisition()
+            
+            data = andor.GetAcquiredData(2 * (xf-xi+1) * (yf-yi+1)).reshape(2,  (yf-yi+1), (xf-xi+1))
+            images = {key: data[i] 
+                      for i, key in enumerate(["image", "bright"])}
+            
+            self._save(images, record_path)
+            self._send_update(record_path, record_type, record_settings)
+        
+        if record_type == 'raw':
+            for i in range(2):
+                andor.StartAcquisition()
+                andor.WaitForAcquisition()
+            
+            data = andor.GetAcquiredData(2 * (xf-xi+1) * (yf-yi+1)).reshape(2,  (yf-yi+1), (xf-xi+1))
+            images = {key: data[i] 
+                      for i, key in enumerate(["bright", "dark"])}
+            
+            self._save(images, record_path)
+            self._send_update(record_path, record_type, record_settings)
+        
+        if record_type == 'gain_calibration':
+            for i in range(3):
+                andor.StartAcquisition()
+                andor.WaitForAcquisition()
+            
+            data = andor.GetAcquiredData(3 * (xf-xi+1) * (yf-yi+1)).reshape(3,  (yf-yi+1), (xf-xi+1))
+            images = {key: data[i] 
+                      for i, key in enumerate(["a", "b", "dark"])}
+            
+            self._save(images, record_path)
+            self._send_update(record_path, record_type, record_settings)
+            
+        print('[camera] Andor iXon-888 recorded')
         
 Device = ixon
